8a8d407146d5cae35c987a3d75376802e839587d
Barry Rowlingson <b.rowlingson@lancs.ac.uk>
2015-10-21 11:19:03 +0100

    added square logo

--- graphics/build.sh
+++ graphics/build.sh
@@ -5,7 +5,7 @@ SVG=ChicasLogo4.svg
 
 echo Build PNG files from SVG source
 
-for id in colour_lettering colour_nolettering bw_lettering bw_nolettering ; do
+for id in square colour_lettering colour_nolettering bw_lettering bw_nolettering ; do
 
 for width in 200 400 600 ; do
 
